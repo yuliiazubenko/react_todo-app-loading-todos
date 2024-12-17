22c2b996db79edd94248c0a08d1a11a2f429a09d
Yuliia Zubenko <95251336+yuliiazubenko@users.noreply.github.com>
2024-12-17 15:59:55 +0100

    solution

---
+++ src/types/Errors.ts
@@ -0,0 +1,7 @@
+export enum ErrorMessages {
+  EmptyTitle = 'Title should not be empty',
+  UnableToAdd = 'Unable to add a todo',
+  UnableToDelete = 'Unable to delete a todo',
+  UnableToLoad = 'Unable to load todos',
+  UnableToUpdate = 'Unable to update a todo',
+}
